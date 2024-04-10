270b692b3d2aee5860b4e0cca7302efabe3c394f
unknown <fishermujahid@gmail.com>
2024-04-10 13:47:14 +0200

    done

--- frontend/src/components/Import.vue
+++ frontend/src/components/Import.vue
@@ -13,16 +13,16 @@
       </form>
     </div>
 
-    <div id="data-container" v-if="uploadedData.length > 0">
+    <div id="data-container" v-if="transformedData.length > 0">
       <h2>Uploaded Data</h2>
       <table>
         <thead>
           <tr>
-            <th v-for="(value, key) in uploadedData[0]" :key="key">{{ key }}</th>
+            <th v-for="(header, index) in headers" :key="index">{{ header }}</th>
           </tr>
         </thead>
         <tbody>
-          <tr v-for="(row, index) in uploadedData" :key="index">
+          <tr v-for="(row, index) in transformedData" :key="index">
             <td v-for="(value, key) in row" :key="key">{{ value }}</td>
           </tr>
         </tbody>
@@ -36,13 +36,10 @@ export default {
   name: 'Import',
   data() {
     return {
-      selectedFile: null
+      selectedFile: null,
+      headers: ['First Name', 'Last Name', 'Personality Type 1', 'Personality Type 2', 'Personality Type 3', 'Personality Type 4', 'Enneagram', 'Main User Persona', 'Secondary User Persona',],
+      transformedData: []
     };
-  },
-  computed: {
-    uploadedData() {
-      return this.$store.getters.uploadedData;
-    }
   },
   methods: {
     handleFileChange(event) {
@@ -64,14 +61,46 @@ export default {
         });
 
         if (!response.ok) {
-          throw new Error("Upload failed.");
+          throw new Error("Upload failed. Please try again.");
         }
 
-        const data = await response.json();
-        this.$store.commit("setUploadedData", data);
+        const excelData = await response.json();
+        console.log('Excel Data:', excelData);
+        if (Array.isArray(excelData)) {
+          this.transformedData = this.transformExcelData(excelData);
+          console.log('Transformed Data:', this.transformedData);
+        } else {
+          throw new Error("Invalid data received from server.");
+        }
+
+        document.getElementById("file").value = "";
       } catch (error) {
-        console.error("Error:", error);
+        console.error("Error:", error.message);
+        alert(error.message);
       }
+    },
+    transformExcelData(data) {
+      const headersRow = data[1];
+      const firstNameIndex = Object.values(headersRow).findIndex(header => header === 'firstName');
+      const lastNameIndex = Object.values(headersRow).findIndex(header => header === 'lastName');
+      const Ptype1Index = Object.values(headersRow).findIndex(header => header === 'personalityType1');
+      const Ptype2Index = Object.values(headersRow).findIndex(header => header === 'personalityType2');
+      const Ptype3Index = Object.values(headersRow).findIndex(header => header === 'personalityType3');
+      const Ptype4Index = Object.values(headersRow).findIndex(header => header === 'personalityType4');
+      const enneagramIndex = Object.values(headersRow).findIndex(header => header === 'enneagram');
+      const mainUpersona = Object.values(headersRow).findIndex(header => header === 'mainUserPersona');
+      const secondaryUpersona = Object.values(headersRow).findIndex(header => header === 'secondaryUserPersona');
+      return data.slice(2).map(row => ({
+        firstName: row[`column_${firstNameIndex + 2}`],
+        lastName: row[`column_${lastNameIndex + 2}`],
+        personalityType1: row[`column_${Ptype1Index + 2}`],
+        personalityType2: row[`column_${Ptype2Index + 2}`],
+        personalityType3: row[`column_${Ptype3Index + 2}`],
+        personalityType4: row[`column_${Ptype4Index + 2}`],
+        enneagram: row[`column_${enneagramIndex + 2}`],
+        mainUserPersona: row[`column_${mainUpersona + 2}`],
+        secondaryUserPersona: row[`column_${secondaryUpersona + 2}`]
+      }));
     }
   }
 }
